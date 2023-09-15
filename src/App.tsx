4fe7e88f4c0a3800a0234a911692c4b5aaa9d32c
amiko <amikchixladze1234@gmail.com>
2023-09-15 19:15:41 +0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import {JSX, useState} from "react";
+import {JSX, useEffect, useState} from "react";
 import Start from "./components/Start.tsx";
 import Finish from "./components/Finish.tsx";
 import Quiz from "./components/Quiz.tsx";
 import * as React from "react";
+import axios from "axios";
+
+interface IData {
+  id: number;
+  question: string;
+  code?: string,
+  correct: string,
+  answers: string[],
+  category: string
+}
 
 export default function App(): JSX.Element {
   const [inputValue, setInputValue] = useState<string>("");
@@ -10,10 +20,11 @@ export default function App(): JSX.Element {
   const [isQuiz, setIsQuiz] = useState<boolean>(false);
   const [isFinish, setIsFinish] = useState<boolean>(false);
 
-  const [questionIndex, setQuestionIndex] = useState<number>(0);
+  const [data, setData] = useState<IData[] | null>(null);
+
+  const [questionIndex, setQuestionIndex] = useState<number>(18);
   //fns
   const inputChange = (event: React.ChangeEvent<HTMLInputElement>): void => setInputValue(event.target.value);
-
 
   const submitForm = (event: React.FormEvent<HTMLFormElement>): void | boolean => {
     event.preventDefault();
@@ -25,6 +36,18 @@ export default function App(): JSX.Element {
       return true;
     }
   }
+  const getQuizes = async (): Promise<void> => {
+    try {
+      const res = await axios.get("http://localhost:8080/quiz");
+      if(res)
+        setData(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  useEffect(() => {
+    getQuizes();
+  }, []);
 
   return (
     <>
@@ -35,7 +58,10 @@ export default function App(): JSX.Element {
             inputChange={inputChange}
             submitForm={submitForm}
           />}
-          {isQuiz && <Quiz />}
+          {isQuiz && <Quiz
+            data={data}
+            questionIndex={questionIndex}
+          />}
           <Finish />
         </div>
       </div>
--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,8 +1,18 @@
 import {JSX} from "react";
 import {motion, AnimatePresence} from "framer-motion";
 import Question from "./Question.tsx";
+import Button from "./Button.tsx";
 
-export default function Quiz(): JSX.Element {
+interface IData {
+  id: number;
+  question: string;
+  code?: string,
+  correct: string,
+  answers: [string],
+  category: string
+}
+
+export default function Quiz({ data, questionIndex }: IData[] | null): JSX.Element {
   return (
     <>
       <AnimatePresence>
@@ -18,13 +28,44 @@ export default function Quiz(): JSX.Element {
           }} exit={{
             scale: 0
           }}
-          className="w-full min-h-[350px] max-h-[550px] rounded-md bg-gray-700"
+          className="w-full min-h-[350px] max-h-[550px] rounded-md bg-gray-700 p-2 py-3"
         >
-          <div className="p-2">
+          <div className="py-3">
             <Question
-              question={"hello how are you?"}
-              classname={"first-letter:font-extrabold font-medium text-center capitalize text-gray-400 text-[20px]"}
+              question={data[questionIndex].question}
+              classname={"first-letter:font-extrabold font-medium text-center capitalize text-gray-400 text-[20px] mb-3"}
             />
+            <code className={""}>
+                {data[questionIndex].code && <pre className={"bg-gray-800 rounded-md px-5 py-3 text-purple-400 w-full border-4 border-gray-900"}>
+                  {data[questionIndex].code}
+                </pre>}
+            </code>
+          </div>
+          <div className="py-2">
+            <ul>
+              {data[questionIndex]?.answers.map((answer) => (
+                <motion.li
+                  initial={{
+                    scale: 1
+                  }} whileTap={{
+                    scale: 0.95
+                  }} transition={{
+                    duration: 0.1,
+                    type: "spring",
+                    stiffness: 250
+                  }}
+                  key={answer}
+                  className={"bg-gray-800 text-gray-400 w-full py-3 px-2 rounded-md cursor-pointer my-2 hover:opacity-90 select-none"}
+                >
+                  {answer}
+                </motion.li>
+              ))}
+            </ul>
+          </div>
+          <div className="">
+            <button className={"px-6 h-10 w-full rounded-md bg-emerald-500 text-gray-300 font-bold text-[18px] capitalize"}>
+              next
+            </button>
           </div>
         </motion.div>
       </AnimatePresence>
